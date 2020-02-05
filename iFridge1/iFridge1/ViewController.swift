dfed7c110aff0f5f8a539aecfa6bba2f2f395f2d
Katelyn Suhr <katy@suhr.com>
2020-02-04 22:54:48 -0800

    Podfile

---
+++ iFridge1/iFridge1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iFridge1
+//
+//  Created by Katelyn Suhr on 2/4/20.
+//  Copyright © 2020 Katelyn Suhr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
